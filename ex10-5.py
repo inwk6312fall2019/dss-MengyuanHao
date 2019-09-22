f84674d7094a4babc065b1fb3a1d66ca1bee7d6a
MengyuanHao <55468472+MengyuanHao@users.noreply.github.com>
2019-09-22 12:30:06 -0300

    Create ex10-5.py

---
+++ ex10-5.py
@@ -0,0 +1,8 @@
+def is_sorted():
+    i = 0
+    while i < len(word) - 1:
+          if word[i] < word[i+1]
+             return True
+          else:
+             return False
+    return t == sorted(t)
