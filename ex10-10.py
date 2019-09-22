689208062b7b7351184373af43222fe617c0d0dc
MengyuanHao <55468472+MengyuanHao@users.noreply.github.com>
2019-09-22 12:41:31 -0300

    Create ex10-10.py

---
+++ ex10-10.py
@@ -0,0 +1,12 @@
+def my_wordlist():
+    my_wordlist = open('words.txt')
+      for line in my_wordlist:
+            word = line.strip()
+            my_wordlist.append(word)
+      return my_wordlist
+def in_bisect(my_wordlist,word):
+    i = in_bisect(my_wordlist,word)
+    if m in i:
+        return index[m]
+    else:
+        return None
