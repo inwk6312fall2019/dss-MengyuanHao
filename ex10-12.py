25149e22d7d56387468122c1d9d937048baf6d59
MengyuanHao <55468472+MengyuanHao@users.noreply.github.com>
2019-09-22 12:43:34 -0300

    Create ex10-12.py

---
+++ ex10-12.py
@@ -0,0 +1,6 @@
+def interlock(wordlist,word):
+    for w in range(n):
+        interlock = word[w::n]
+         if not in_bisect(my_wordlist,interlock):
+                return False
+    return True
