81c89cc2641fa6393d4e7ab59cae2479a0be9388
MengyuanHao <55468472+MengyuanHao@users.noreply.github.com>
2019-09-22 12:52:59 -0300

    Create ex12-1.py

---
+++ ex12-1.py
@@ -0,0 +1,8 @@
+def most_frequenr(f):
+    histogram = word list
+    ans = []
+    for char in histogram:
+        if char[i+1] < char[i]
+           return True
+        ans.append()
+    return False
