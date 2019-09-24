afc3bb15beece8e26be3ab7e3270b3dcfcbd0687
MengyuanHao <55468472+MengyuanHao@users.noreply.github.com>
2019-09-24 11:51:48 -0300

    Create ex13-4.py

---
+++ ex13-4.py
@@ -0,0 +1,17 @@
+myprocess = open("file")
+pass
+
+for word in list_of_words:
+    if word not in given_wordlist:
+       newlist.append(word)
+def hamming_fun(this_word,other_word):
+    if len(this_word) != len(other_word)
+    return -1
+    hamming = 0
+    for index,char in enumerate(this_word), this_word:
+    for i in range(len(this_word)):
+        if this_word[i] == other_word[i]:
+           pass
+        else:
+           hamming += 1
+    return hamming
